2655d0313e8dd720ac05b5feb59ff41751730cd4
Joey Wynne <joey.wunne@btinternet.com>
2025-08-04 14:07:20 +0100

    add recursive validity checker

--- game_solvers/skyscraper_logic_puzzle/skyscraper_solver.py
+++ game_solvers/skyscraper_logic_puzzle/skyscraper_solver.py
@@ -3,6 +3,7 @@ import numpy as np
 from game_solvers.logger import LOG
 from game_solvers.sporcle_parser import DOWNLOAD_BASE_PATH
 from pathlib import Path
+from itertools import product
 
 
 SKYSCRAPER_PUZZLES_PATH = DOWNLOAD_BASE_PATH / "skyscraper_logic_puzzles"
@@ -16,11 +17,16 @@ def solve_board(board: Board) -> bool:
         changed = any(
             [
                 square_has_one_possible_value(board),
-                value_in_group_has_one_possible_square(board)
+                value_in_group_has_one_possible_square(board),
             ]
         )
-        if not changed:
+        if changed:
+            continue
+
+        # Last one to try
+        if not if_rule_try_the_options(board):
             break
+
 
     return board.is_solved
 
@@ -28,7 +34,6 @@ def solve_board(board: Board) -> bool:
 def square_has_one_possible_value(board: Board) -> bool:
     """If a square only has one possible value, assign it."""
     # Check all squares
-    LOG.info("Checking squares have one possible")
     updated = False
     for square in board.all_squares(active=True):
         sub_values = square.sub_values
@@ -72,6 +77,69 @@ def value_in_group_has_one_possible_square(board: Board) -> bool:
     return updated
 
 
+def if_rule_try_the_options(board: Board) -> bool:
+    """If we have a rule and squares available - try all options."""
+    updated = False
+    for direction, rules in board.visible_buildings.items():
+        for idx, rule in enumerate(rules):
+            if rule == 0:
+                # 0 represents no rule
+                continue
+            group = board.get_group(direction, idx)
+            unplaced = np.where([g.shape_value == 0 for g in group])[0]
+            if len(unplaced) == 0:
+                # Group is complete
+                continue
+            valid_options = attempt_to_fit_buildings(group, unplaced, rule)
+            
+            for id, option in valid_options.items():
+                square = group[id]
+                if square.sub_values != list(option):
+                    updated = True
+                    # TODO improve this log message (detail)
+                    LOG.info("Updating group with valid options")
+                    square.sub_values = list(option)
+    return updated
+
+
+
+def attempt_to_fit_buildings(group: np.ndarray, unplaced: np.ndarray, rule: int) -> dict:
+    """Attempt to recursively find a solution for a row.
+    
+    Return a dict of idx -> sub_values.
+    """
+    results = {u: set() for u in unplaced}
+    sub_values = (g.sub_values for g in group[unplaced])
+    all_candidates = product(*sub_values)
+    # Filter out all_candidates with duplicates
+    all_candidates = [sublist for sublist in all_candidates if len(sublist) == len(set(sublist))]
+    
+    for candidate in all_candidates:
+        unplaced_iter = iter(candidate)
+        test_group = [
+            g.shape_value if i not in unplaced else next(unplaced_iter)
+            for i, g in enumerate(group)
+        ]
+        is_valid = (calc_number_buildings_seen(test_group) == rule)
+        if not is_valid:
+            continue
+
+        for i, candidate in enumerate(test_group):
+            if i in results:
+                results[i].add(candidate)
+    return results
+
+
+def calc_number_buildings_seen(building_group: np.ndarray) -> int:
+    """Calculate the number of buildings seen from the start."""
+    highest_seen = building_group[0]
+    count = 1
+    for building in building_group[1:]:
+        if building > highest_seen:
+            count += 1
+            highest_seen = building
+    return count
+
 def buildings_seen_limits_max_square_value(board: Board) -> None:
     """If x buildings seen then tallest building in square one is 2 + N - r + i
     N = Grid size
@@ -94,13 +162,6 @@ def buildings_seen_limits_max_square_value(board: Board) -> None:
             group = board.get_group(direction, idx)
             for idx, g in enumerate(group):
                 g.sub_values = [s for s in g.sub_values if s <= max_allowed_value + idx]
-
-
-    # Im missing something with this limiting thing.
-
-    #e.g. 4 [][][][] -> [1][2][3][4]
-    # 1 [][][][] -> [4][123][123][123]
-    # 2 [][][][] -> [123][]
 
 
 if __name__ == "__main__":
